0eea740af23b2869e746441a4fff78412b5c12ac
santijll <santijll@example.com>
2025-05-13 17:38:34 +0200

    añade traducciones

--- src/app/[locale]/favoritos/page.tsx
+++ src/app/[locale]/favoritos/page.tsx
@@ -2,11 +2,13 @@ import { getFavoriteFlashcardsByUserId } from "@/lib/dbqueries/getFavoriteByFlas
 import { getUserByEmail } from "@/lib/dbqueries/getUser";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 import { FlashcardFavoritesClient } from "./FlashcardFavoritesClient";
 
 export default async function FavoritosPage() {
   const { getUser } = getKindeServerSession();
+  const t = useTranslations("Favorites");
   const user = await getUser();
 
   if (!user?.email) return notFound();
@@ -19,7 +21,7 @@ export default async function FavoritosPage() {
   return (
     <>
       <div className="flex justify-between items-center px-8 py-5">
-        <h1 className="text-3xl font-bold">Mis Favoritos</h1>
+        <h1 className="text-3xl font-bold">t("myfavorites")</h1>
       </div>
 
       <FlashcardFavoritesClient flashcards={flashcards} userId={dbUser.id} />
